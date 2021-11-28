f8e983023852782be027356cbc7709d61ccf5be4
张海涛(ZhangHaiTao)-顺丰科技技术集团 <>
2021-11-28 17:14:41 +0800

    commit-0.0.1

---
+++ Classes/PrintObject.h
@@ -0,0 +1,18 @@
+//
+//  PrintObject.h
+//  SFBug
+//
+//  Created by 张海涛(ZhangHaiTao)-顺丰科技技术集团 on 2021/11/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PrintObject : NSObject
+
++ (void)print;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/PrintObject.m
@@ -0,0 +1,16 @@
+//
+//  PrintObject.m
+//  SFBug
+//
+//  Created by 张海涛(ZhangHaiTao)-顺丰科技技术集团 on 2021/11/28.
+//
+
+#import "PrintObject.h"
+
+@implementation PrintObject
+
++ (void)print{
+    NSLog(@"this is test");
+}
+
+@end
